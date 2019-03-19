89656db2392982fe9f259ea7cb8f769b500c3dd5
manuel alonso <manuel@manuels-MacBook-Pro.local>
2019-03-19 09:55:30 +0100

    Vulnerabilities fixed

--- node_modules/react-scripts/config/jest/fileTransform.js
+++ node_modules/react-scripts/config/jest/fileTransform.js
@@ -1,11 +1,3 @@
-// @remove-on-eject-begin
-/**
- * Copyright (c) 2014-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-// @remove-on-eject-end
 'use strict';
 
 const path = require('path');
@@ -15,6 +7,25 @@ const path = require('path');
 
 module.exports = {
   process(src, filename) {
-    return `module.exports = ${JSON.stringify(path.basename(filename))};`;
+    const assetFilename = JSON.stringify(path.basename(filename));
+
+    if (filename.match(/\.svg$/)) {
+      return `const React = require('react');
+      module.exports = {
+        __esModule: true,
+        default: ${assetFilename},
+        ReactComponent: React.forwardRef((props, ref) => ({
+          $$typeof: Symbol.for('react.element'),
+          type: 'svg',
+          ref: ref,
+          key: null,
+          props: Object.assign({}, props, {
+            children: ${assetFilename}
+          })
+        })),
+      };`;
+    }
+
+    return `module.exports = ${assetFilename};`;
   },
 };
